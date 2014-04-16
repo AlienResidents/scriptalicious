011444257d1600755e74842396871f176027f7b5
Chris Donovan <alienresidents@gmail.com>
2014-04-16 12:47:36 +1000

    Update jira-resetAllPasswords.pl

--- jira_stuff/jira-resetAllPasswords.pl
+++ jira_stuff/jira-resetAllPasswords.pl
@@ -39,8 +39,7 @@ my @chars = ( '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c',
 
 # This is a case sensitive list of usernames to ignore processing for
 my @userIgnore = (
-  'Chris.Donovan',
-  'Richard.Stevenson',
+
 );
 
 # Start your vars!
